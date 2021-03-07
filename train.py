7da9b58ed1c8bc43d1dc4e24ada927f5a712e491
Noman-Tanveer <nomantanveer021@gmail.com>
2021-03-07 23:14:04 +0500

    Edited train.py

--- train.py
+++ train.py
@@ -1,1 +1,265 @@
 print('train.py Contaains the core training loop')
+print('train.py Contaains the core training loop')
+
+import torch
+from torch import nn
+import torchvision
+import matplotlib
+import numpy
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+
+
+class Conv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, relu=True, same_padding=False, bn=False):
+        super(Conv2d, self).__init__()
+        padding = int((kernel_size - 1) / 2) if same_padding else 0
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=padding)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0, affine=True) if bn else None
+        self.relu = nn.ReLU(inplace=True) if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+class FC(nn.Module):
+    def __init__(self, in_features, out_features, relu=True):
+        super(FC, self).__init__()
+        self.fc = nn.Linear(in_features, out_features)
+        self.relu = nn.ReLU(inplace=True) if relu else None
+
+    def forward(self, x):
+        x = self.fc(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+def save_net(fname, net):
+    import h5py
+    h5f = h5py.File(fname, mode='w')
+    for k, v in net.state_dict().items():
+        h5f.create_dataset(k, data=v.cpu().numpy())
+
+
+def load_net(fname, net):
+    import h5py
+    h5f = h5py.File(fname, mode='r')
+    for k, v in net.state_dict().items():
+        param = torch.from_numpy(np.asarray(h5f[k]))
+        v.copy_(param)
+
+
+def load_pretrained_npy(faster_rcnn_model, fname):
+    params = np.load(fname).item()
+    # vgg16
+    vgg16_dict = faster_rcnn_model.rpn.features.state_dict()
+    for name, val in vgg16_dict.items():
+        # # print name
+        # # print val.size()
+        # # print param.size()
+        if name.find('bn.') >= 0:
+            continue
+        i, j = int(name[4]), int(name[6]) + 1
+        ptype = 'weights' if name[-1] == 't' else 'biases'
+        key = 'conv{}_{}'.format(i, j)
+        param = torch.from_numpy(params[key][ptype])
+
+        if ptype == 'weights':
+            param = param.permute(3, 2, 0, 1)
+
+        val.copy_(param)
+
+    # fc6 fc7
+    frcnn_dict = faster_rcnn_model.state_dict()
+    pairs = {'fc6.fc': 'fc6', 'fc7.fc': 'fc7'}
+    for k, v in pairs.items():
+        key = '{}.weight'.format(k)
+        param = torch.from_numpy(params[v]['weights']).permute(1, 0)
+        frcnn_dict[key].copy_(param)
+
+        key = '{}.bias'.format(k)
+        param = torch.from_numpy(params[v]['biases'])
+        frcnn_dict[key].copy_(param)
+
+
+def np_to_variable(x, is_cuda=True, dtype=torch.FloatTensor):
+    v = Variable(torch.from_numpy(x).type(dtype))
+    if is_cuda:
+        v = v.cuda()
+    return v
+
+
+def set_trainable(model, requires_grad):
+    for param in model.parameters():
+        param.requires_grad = requires_grad
+
+
+def weights_normal_init(model, dev=0.01):
+    if isinstance(model, list):
+        for m in model:
+            weights_normal_init(m, dev)
+    else:
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(0.0, dev)
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0.0, dev)
+
+# hyper-parameters
+# ------------
+imdb_name = 'voc_2007_trainval'
+cfg_file = 'experiments/cfgs/faster_rcnn_end2end.yml'
+pretrained_model = 'data/pretrained_model/VGG_imagenet.npy'
+output_dir = 'models/saved_model3'
+
+start_step = 0
+end_step = 100000
+lr_decay_steps = {60000, 80000}
+lr_decay = 1./10
+
+rand_seed = 1024
+_DEBUG = True
+use_tensorboard = True
+remove_all_log = False   # remove all historical experiments in TensorBoard
+exp_name = None # the previous experiment name in TensorBoard
+
+# ------------
+
+if rand_seed is not None:
+    np.random.seed(rand_seed)
+
+# load config
+cfg_from_file(cfg_file)
+lr = cfg.TRAIN.LEARNING_RATE
+momentum = cfg.TRAIN.MOMENTUM
+weight_decay = cfg.TRAIN.WEIGHT_DECAY
+disp_interval = cfg.TRAIN.DISPLAY
+log_interval = cfg.TRAIN.LOG_IMAGE_ITERS
+
+# load data
+imdb = get_imdb(imdb_name)
+rdl_roidb.prepare_roidb(imdb)
+roidb = imdb.roidb
+data_layer = RoIDataLayer(roidb, imdb.num_classes)
+
+# load net
+net = FasterRCNN(classes=imdb.classes, debug=_DEBUG)
+network.weights_normal_init(net, dev=0.01)
+network.load_pretrained_npy(net, pretrained_model)
+# model_file = '/media/longc/Data/models/VGGnet_fast_rcnn_iter_70000.h5'
+# model_file = 'models/saved_model3/faster_rcnn_60000.h5'
+# network.load_net(model_file, net)
+# exp_name = 'vgg16_02-19_13-24'
+# start_step = 60001
+# lr /= 10.
+# network.weights_normal_init([net.bbox_fc, net.score_fc, net.fc6, net.fc7], dev=0.01)
+
+net.cuda()
+net.train()
+
+params = list(net.parameters())
+# optimizer = torch.optim.Adam(params[-8:], lr=lr)
+optimizer = torch.optim.SGD(params[8:], lr=lr, momentum=momentum, weight_decay=weight_decay)
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# tensorboad
+use_tensorboard = use_tensorboard and CrayonClient is not None
+if use_tensorboard:
+    cc = CrayonClient(hostname='127.0.0.1')
+    if remove_all_log:
+        cc.remove_all_experiments()
+    if exp_name is None:
+        exp_name = datetime.now().strftime('vgg16_%m-%d_%H-%M')
+        exp = cc.create_experiment(exp_name)
+    else:
+        exp = cc.open_experiment(exp_name)
+
+# training
+train_loss = 0
+tp, tf, fg, bg = 0., 0., 0, 0
+step_cnt = 0
+re_cnt = False
+t = Timer()
+t.tic()
+for step in range(start_step, end_step+1):
+
+    # get one batch
+    blobs = data_layer.forward()
+    im_data = blobs['data']
+    im_info = blobs['im_info']
+    gt_boxes = blobs['gt_boxes']
+    gt_ishard = blobs['gt_ishard']
+    dontcare_areas = blobs['dontcare_areas']
+
+    # forward
+    net(im_data, im_info, gt_boxes, gt_ishard, dontcare_areas)
+    loss = net.loss + net.rpn.loss
+
+    if _DEBUG:
+        tp += float(net.tp)
+        tf += float(net.tf)
+        fg += net.fg_cnt
+        bg += net.bg_cnt
+
+    train_loss += loss.data[0]
+    step_cnt += 1
+
+    # backward
+    optimizer.zero_grad()
+    loss.backward()
+    network.clip_gradient(net, 10.)
+    optimizer.step()
+
+    if step % disp_interval == 0:
+        duration = t.toc(average=False)
+        fps = step_cnt / duration
+
+        log_text = 'step %d, image: %s, loss: %.4f, fps: %.2f (%.2fs per batch)' % (
+            step, blobs['im_name'], train_loss / step_cnt, fps, 1./fps)
+        log_print(log_text, color='green', attrs=['bold'])
+
+        if _DEBUG:
+            log_print('\tTP: %.2f%%, TF: %.2f%%, fg/bg=(%d/%d)' % (tp/fg*100., tf/bg*100., fg/step_cnt, bg/step_cnt))
+            log_print('\trpn_cls: %.4f, rpn_box: %.4f, rcnn_cls: %.4f, rcnn_box: %.4f' % (
+                net.rpn.cross_entropy.data.cpu().numpy()[0], net.rpn.loss_box.data.cpu().numpy()[0],
+                net.cross_entropy.data.cpu().numpy()[0], net.loss_box.data.cpu().numpy()[0])
+            )
+        re_cnt = True
+
+    if use_tensorboard and step % log_interval == 0:
+        exp.add_scalar_value('train_loss', train_loss / step_cnt, step=step)
+        exp.add_scalar_value('learning_rate', lr, step=step)
+        if _DEBUG:
+            exp.add_scalar_value('true_positive', tp/fg*100., step=step)
+            exp.add_scalar_value('true_negative', tf/bg*100., step=step)
+            losses = {'rpn_cls': float(net.rpn.cross_entropy.data.cpu().numpy()[0]),
+                      'rpn_box': float(net.rpn.loss_box.data.cpu().numpy()[0]),
+                      'rcnn_cls': float(net.cross_entropy.data.cpu().numpy()[0]),
+                      'rcnn_box': float(net.loss_box.data.cpu().numpy()[0])}
+            exp.add_scalar_dict(losses, step=step)
+
+    if (step % 10000 == 0) and step > 0:
+        save_name = os.path.join(output_dir, 'faster_rcnn_{}.h5'.format(step))
+        network.save_net(save_name, net)
+        print('save model: {}'.format(save_name))
+    if step in lr_decay_steps:
+        lr *= lr_decay
+        optimizer = torch.optim.SGD(params[8:], lr=lr, momentum=momentum, weight_decay=weight_decay)
+
+    if re_cnt:
+        tp, tf, fg, bg = 0., 0., 0, 0
+        train_loss = 0
+        step_cnt = 0
+        t.tic()
+        re_cnt = False
